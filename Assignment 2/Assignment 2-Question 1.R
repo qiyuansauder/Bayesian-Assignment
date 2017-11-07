4d1023863e5154c0dfd0822428a31a014e354e01
qiyuansauder <33267682+qiyuansauder@users.noreply.github.com>
2017-11-07 14:09:17 -0800

    Add files via upload

---
+++ Assignment 2/Assignment 2-Question 1.R
@@ -0,0 +1,88 @@
+######################################
+###COMM 590  Assignment 2 Question 1
+###Qiyuan Wang
+######################################
+
+# Library for sampling from Multivariate Normal distribution
+require(mvtnorm)
+# Library for sampling from Truncated Normal distribution
+require(truncnorm)
+library(MASS)
+library(ggplot2)
+library(reshape2)
+
+
+###Binary Probit Model
+BiProbit=function(X,Y,N,mean,variance){
+  #This function use Gibbs Sampler to draw from posterior distribution for linear models
+  #X,Y are idependent variables matrix and Y is dependent variable vector
+  #N is the number of iteration
+  #mean and variance are the hyper parameters for regression coefficients' normal prior
+  #alpha and beta are the hyper parameters for standard error' gamma prior
+  
+  nparams=ncol(X)+1#number of parameters to be estimated=number of DV+intercept+latent variable
+  nobs=nrow(X)#number of observations
+  X=cbind(matrix(1,nrow=nobs,ncol=1),X)
+  
+  #set the matrix to store draws for beta
+  draw=matrix(0,nrow=N,ncol=nparams)
+  #set the matrix tos tore draws for latent variable
+  latent=matrix(0,nrow=nobs,ncol=1)
+  
+  #set the starting value for draws
+  draw[1,]=1
+  
+  
+  #Gibbs Sampler
+  for(i in 2:N){
+    #Firstly, draw for latent variable from truncated normal distribution
+    #the mean for truncated normal distribution
+    meantru=X%*%draw[i-1,1:nparams]
+    #the estimate for OLS
+    latent[which(Y==1),1]=rtruncnorm(1, a=0, b=Inf, mean = meantru[which(Y==1),1], sd = 1)
+    latent[which(Y==0),1]=rtruncnorm(1, a=-Inf, b=0, mean = meantru[which(Y==0),1], sd = 1)
+    
+    #Secondly, draw for beta from normal distribution
+    #calculate the mean and variance for posterior distribution
+    meantheta=solve(t(X)%*%X+solve(variance))%*%(t(X)%*%latent+solve(variance)%*%mean)
+    variancetheta=solve(t(X)%*%X+solve(variance))
+    #draw from multivariate normal distribution
+    draw[i,]=mvrnorm(1,meantheta,variancetheta)
+    
+    #show the iteration results
+    cat(paste("Iteration",i,sep=' '),":",draw[i,], "\n")
+  }
+  return(list(draw,variancetheta))
+}
+
+
+##############test the model
+#generate data
+beta=c(-1,0.5,1.5)
+#generate X
+X=rnorm(2000)
+X=matrix(X,nrow=1000)
+Xtemp=cbind(rep(1,times=1000),X)
+#chocie probability
+PChoice=pnorm(Xtemp%*%beta)
+temp=runif(1000)
+Y=as.matrix(as.numeric(temp<=PChoice))
+
+#run the model
+N=10000
+mean=c(10,10,10)
+variance=diag(c(5,5,5))
+test=BiProbit(X,Y,N,mean,variance)
+
+#esimate
+ThetaEstimate=test[[1]]
+ThetaVariance=test[[2]]
+
+colMeans(ThetaEstimate)
+ThetaEstimate=as.data.frame(ThetaEstimate)
+names(ThetaEstimate)=c('Theta1','Theta2','Theta3')
+ThetaEstimate$Iteration=1:10000
+ThetaEstimate=melt(ThetaEstimate,id.vars = 4)
+names(ThetaEstimate)=c('Iteration','Variable','Value')
+qplot(Iteration,Value,data=ThetaEstimate,color=Variable)
+
---
+++ Assignment 2/Assignment 2-Question 2.R
@@ -0,0 +1,144 @@
+######################################
+###COMM 590  Assignment 1 Question 2
+###Qiyuan Wang
+######################################
+library(mvtnorm)
+
+#Metropolis-Hastings Algorithm for Multinomial Logit Model
+#in this model we assume number of alternatives in each choice is the same
+#and there is not outside option
+
+#choice probability
+PChoice=function(X,theta,Choice){
+  #number of choices made in the data
+  noption=nrow(X)/length(Choice)
+  nchoice=length(Choice)
+  #utility of each option
+  Utility=exp(X%*%theta)
+  #reshape into nchoice*noption matrix
+  temp=matrix(Utility,ncol=noption,byrow=T)
+  #calculate the choice probability
+    #the index for choose option
+    ind=cbind(1:nchoice,Choice)
+    #do log transformation here because the product of 1000 probability will be 0
+    choiceprob=sum(log(temp[ind]/(rowSums(temp))))
+  
+  return(choiceprob)
+}
+
+#prior probability
+PPrior=function(theta,mean,variance){
+  PPrior=log(dmvnorm(theta,mean=mean,sigma=variance))
+  return(PPrior)
+}
+
+#posterior probability
+targetRelProb=function(X,theta,Choice,mean,variance){
+  PPosterior=PChoice(X,theta,Choice)+PPrior(theta,mean,variance)
+  return(PPosterior)
+}
+
+
+#the main function
+MultiNomial=function(X,Choice,N,mean,variance){
+  #X is independent variable
+  #Y is dependent variable, namely which option is chosen
+  #mean and variance is the parameter for normal posterior
+
+  #number of parameters
+  nparams=ncol(X)
+  
+  #construct matrix to store iteration results
+  draw=matrix(0,nrow=N,ncol=nparams)
+  #set the starting value
+  draw[1,]=1
+  
+  thetaold=draw[1,]
+  #the iteration for HM algorithm
+  for(i in 2:N){
+    #loop over the parameters
+    for(j in 1:nparams){
+      #using random walk propose
+      thetaprime=rnorm(1,mean=0,sd=2)
+      #proposed theta
+      thetanew=thetaold
+      thetanew[j]=thetanew[j]+thetaprime
+      
+      #calculate the probability of accepting
+      PAccept=min(c(1,exp(targetRelProb(X,thetanew,Choice,mean,variance)-
+                      targetRelProb(X,thetaold,Choice,mean,variance))))
+      
+      if(runif(1)<=PAccept){
+        thetaold=thetanew
+      }
+      else{
+        thetanew=thetaold
+      }
+    }
+    draw[i,]=thetanew
+    if(i<=100){
+      cat("Iteration",i,": ",draw[i,], "\n")
+    }else{
+      #every 100 iteration print the results
+      if(i%%100==0){
+        cat("Iteration",i,": ",draw[i,], "\n")
+      }
+    }
+  }
+  return(draw)
+}
+
+
+###test the model
+#generate data
+#In the dataset, there are 3 IV (including intercept).
+#For each choice, there are 3 options
+theta=c(0.5,1)
+X=matrix(rnorm(6000),ncol=2)
+
+#calculate utility and choice probability
+temputility=exp(X%*%theta)
+temputility=matrix(temputility,ncol=3,byrow=T)
+tempPchoice=temputility/rowSums(temputility)
+
+#make the choice
+Choice=numeric(1000)
+tempPchoice[,2]=tempPchoice[,2]+tempPchoice[,1]
+tempP=runif(1000)
+for (i in 1:1000){
+  if(tempP[i]<=tempPchoice[i,1]){
+    Choice[i]=1
+  }
+  else if(tempP[i]<=tempPchoice[i,2]){
+    Choice[i]=2
+  }
+  else{
+    Choice[i]=3
+  }
+}
+
+#run the code
+mean=c(1,1)
+variance=diag(c(1,1))
+N=10000
+test=MultiNomial(X,Choice,N,mean,variance)
+
+colMeans(test)
+test=as.data.frame(test)
+names(test)=c('Beta1','Beta2')
+test$Iteration=1:10000
+test=melt(test,id.vars = 3)
+names(test)=c('Iteration','Variable','Value')
+qplot(Iteration,Value,data=test,color=Variable)
+
+#calculate autocorrelation
+test=test[order(test$Variable,test$Iteration),]
+
+test$lag1=c(1,test$Value[1:19999])
+test$lag10=c(rep(1,times=10),test$Value[1:19990])
+test$lag50=c(rep(1,times=50),test$Value[1:19950])
+test$lag100=c(rep(1,times=100),test$Value[1:19900])
+cor.test(test$Value,test$lag1)
+cor.test(test$Value,test$lag10)
+cor.test(test$Value,test$lag50)
+cor.test(test$Value,test$lag100)
